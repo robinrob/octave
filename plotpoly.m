8f090dbd8296b08060351dc753c27be8c8211f04
robinrob <robin@mrrobinsmith.com>
2015-09-15 22:51:52 +0100

    Quick-save.

--- plotpoly.m
+++ plotpoly.m
@@ -1,25 +1,28 @@
 function plotpoly(coefficients)
-  x = [-10:0.5:10];
+  x = linspace(-4, 4, 40);
+
   y = polyval(coefficients, x);
 
   plot(x, y, "linewidth", 5, "color", "red")
 
-  range = max(y) - min(y);
-  interval = round(range / 10);
+  x_interval = (max(x) - min(x)) / 20;
+
+  y_range = max(y) - min(y);
+  y_interval = round(y_range / 10);
 
   set(gca, "yaxislocation", "zero")
   set(gca, "xaxislocation", "zero")
 
-  set(gca, "ytick", [min(y):interval:max(y)])
+  set(gca, "ytick", round(min(y):y_interval:max(y)))
   set(gca, "ylim", [min(0, min(y)) max(y)])
 
-  set(gca, "xtick", [-10:1:10])
+  set(gca, "xtick", min(x):x_interval:max(x))
 
   set(gca, "linewidth", 1)
   set(gca, "fontsize", 18)
 
-  xlabel("x", "fontsize", 30)
-  ylabel("y", "fontsize", 30)
+  % xlabel("x", "fontsize", 20)
+  % ylabel("y", "fontsize", 20)
 
   % grid on
   box("off")
@@ -28,7 +31,7 @@ function plotpoly(coefficients)
   for index=1:1:length(coefficients)
     c_string = sprintf("%s %.0f x^%d +", c_string, coefficients(index), length(coefficients) - index);
   end
-  c_string = c_string(1:length(c_string)-1)
+  c_string = c_string(1:length(c_string)-1);
 
   title(sprintf("x-y plot of: %s", c_string), "fontsize", 20);
 end
